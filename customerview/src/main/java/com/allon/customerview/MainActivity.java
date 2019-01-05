650dfad67d58e54ed344a674b4d1a03071a0f419
allon <an_longlong92@163.com>
2019-01-05 09:47:25 +0800

    在mac上面修改了一些配置信

--- customerview/src/main/java/com/allon/customerview/MainActivity.java
+++ customerview/src/main/java/com/allon/customerview/MainActivity.java
@@ -31,32 +31,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.tv_base_view_knowladge:
                 BaseViewKnowledgeActivity.start(this);
-                OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
-                Retrofit retrofit = new Retrofit.Builder().
-                        addConverterFactory(GsonConverterFactory.create())
-                        .client(okHttpClient).baseUrl("http://118.190.86.237/fireInspection/php/")
-                        .build();
-                LoginParams loginParams = new LoginParams();
-                loginParams.setPassword("96e79218965eb72c92a549dd5a330112");
-                loginParams.setName("13333333333");
-                retrofit.create(ApiService.class).method(loginParams.getUrl(), loginParams).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        System.out.println("call = [" + call + "], response = [" + response + "]");
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        System.out.println("call = [" + call + "], t = [" + t + "]");
-                    }
-                });
                 break;
                 default:
         }
     }
 
-    public interface ApiService {
-        @POST
-        public Call<ResponseBody> method(@Url String url, @Body LoginParams loginParams);
-    }
 }
