d0edf723731452a300c7b8689802f2d46d70ed12
allon <an_longlong92@163.com>
2019-02-24 12:37:55 +0800

    让通过windowManager添加的view随着手指的滑动而滑动

--- windowandwindowmanagerdemo/src/main/java/com/example/windowandwindowmanagerdemo/MainActivity.java
+++ windowandwindowmanagerdemo/src/main/java/com/example/windowandwindowmanagerdemo/MainActivity.java
@@ -5,11 +5,17 @@ import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private WindowManager.LayoutParams mParams;
+    private WindowManager mWindowManager;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +31,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addButtonUseWindow() {
-        Button button = new Button(getBaseContext());
-        button.setText("button");
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, 1, 0, PixelFormat.TRANSPARENT);
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE|WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-        params.x = 100;
-        params.y = 300;
-        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        windowManager.addView(button, params);
+        if (null != mButton) return;
+        mButton = new Button(getBaseContext());
+        mButton.setText("button");
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "lllll", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mButton.setOnTouchListener(this);
+        mParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, 1, 0, PixelFormat.TRANSPARENT);
+        mParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL|WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+        mParams.gravity = Gravity.TOP | Gravity.LEFT;
+        mParams.x = 100;
+        mParams.y = 300;
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        mWindowManager.addView(mButton, mParams);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                mParams.x = rawX;
+                mParams.y = rawY;
+                mWindowManager.updateViewLayout(mButton, mParams);
+                break;
+        }
+        return false;
     }
 }
